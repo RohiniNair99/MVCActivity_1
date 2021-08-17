b622d654bd85f6c7f6edd8e189d8df4561e2d3be
Rohini99 <rohini.nair@ltts.com>
2021-08-17 18:04:44 +0530

    pushed

---
+++ ActivityMVC/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActivityMVC.Controllers
+{
+
+    public class MovieController : Controller
+    {
+            
+            // GET: Movie
+            public ActionResult Index()
+            {
+                Models.Movie obj=new Models.Movie();
+               
+                var lstr = obj.GetDetail();
+                return View(lstr);
+            }
+
+        }
+    }
+
---
+++ ActivityMVC/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ActivityMVC.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        public List<Models.Movie> GetDetail()
+        {
+            List<Models.Movie> lst = new List<Models.Movie>();
+            lst.Add(new Models.Movie { ID = 1, Title = "Digital Fortress", Price = 120 });
+            lst.Add(new Models.Movie { ID = 2, Title = "Angels and Demons", Price = 140 });
+            lst.Add(new Models.Movie { ID = 3, Title = "The subtle art of not giving a fuck", Price = 110 });
+            return lst;
+        }
+    }
+
+ 
+   
+}
